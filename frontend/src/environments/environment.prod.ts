c8c182226e509baae9016300c5965d0a52936e3d
Réka Ehreth <reka.ehreth@gmail.com>
2021-10-01 22:42:00 +0200

    Separate service from client

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    mapbox: {
-        accessToken: 'pk.eyJ1IjoicmVrYWVocmV0aCIsImEiOiJja3A0bG9hMzUwbThlMnByMnViendheHRxIn0.mrjV-RRK8SlpxwwI_XMekw'
-    }
-};
