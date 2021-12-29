ca94f74b62106e8df88e851a67c007868139e546
Reka Ehreth <reka.ehreth@emarsys.com>
2021-12-29 23:55:10 +0100

    feat(service-port): Move service port to environment variable

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         logger: ['log', 'error', 'warn', 'debug']
     });
     app.enableCors();
-    await app.listen(3000);
+    await app.listen(process.env.APP_PORT);
 }
 bootstrap();
